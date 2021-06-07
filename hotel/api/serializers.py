8be433d7d958f4135aa59f5c3885df8c54af4dd6
Shay Kekae <mkekae@gmail.com>
2021-06-07 16:01:38 +0200

    first commit

---
+++ hotel/api/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework.serializers import(
+    HyperlinkedIdentityField,
+    ModelSerializer
+)
+
+from hotel.models import Room
+
+
+class RoomCreateUpdateSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = [
+            'id',
+            'room_num',
+            'guest',
+            'check_in',
+            'check_out'
+
+        ]
+
+
+class RoomListSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = [
+            'id',
+            'room_num',
+            'guest',
+            'check_in',
+            'check_out'
+
+        ]
+
+
+class RoomDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = [
+            'id',
+            'room_num',
+            'guest',
+            'check_in',
+            'check_out'
+
+        ]
